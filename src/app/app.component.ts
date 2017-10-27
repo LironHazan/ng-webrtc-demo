50932e527d100d37ba1b162c29c77df1ef4060b1
lironh <lironenk@gmail.com>
2017-10-27 17:04:39 +0300

    chore(css changes)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,6 @@ import {Component, OnInit} from '@angular/core';
 export class AppComponent implements OnInit {
   format = 'video/webm';
   constrains = {video: true, audio: false};
-  showVideoPlayer = true;
 
   constructor() {}
 
